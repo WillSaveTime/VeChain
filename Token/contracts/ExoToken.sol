4d8e1b2ff1df5b57189a42c6d635159bdb1bd895
Caesar0910 <cooker98910@gmail.com>
2022-05-31 00:47:19 +0900

    first

---
+++ Token/contracts/ExoToken.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/common/ERC2981Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract ExoToken is
+	Initializable,
+	ERC20Upgradeable,
+	ERC20BurnableUpgradeable,
+	PausableUpgradeable,
+	OwnableUpgradeable,
+	ERC20PermitUpgradeable,
+	ERC20VotesUpgradeable,
+	ERC2981Upgradeable
+{
+    /// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() initializer {}
+
+    function initialize() public initializer {
+        __ERC20_init("ExoToken", "EXO");
+        __ERC2981_init();
+		__Pausable_init();
+		__Ownable_init();
+    }
+
+	function pause() public onlyOwner {
+		_pause();
+	}
+
+	function unpause() public onlyOwner {
+		_unpause();
+	}
+
+	function mint(address to, uint256 amount) public onlyOwner {
+		_mint(to, amount);
+	}
+
+	function _beforeTokenTransfer(
+		address from,
+		address to,
+		uint256 amount
+	) internal override whenNotPaused {
+		super._beforeTokenTransfer(from, to, amount);
+	}
+
+	// The following functions are overrides required by Solidity.
+
+	function _afterTokenTransfer(
+		address from,
+		address to,
+		uint256 amount
+	) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+		super._afterTokenTransfer(from, to, amount);
+	}
+
+	function _mint(address to, uint256 amount)
+		internal
+		override(ERC20Upgradeable, ERC20VotesUpgradeable)
+	{
+		super._mint(to, amount);
+	}
+
+	function _burn(address account, uint256 amount)
+		internal
+		override(ERC20Upgradeable, ERC20VotesUpgradeable)
+	{
+		super._burn(account, amount);
+	}
+}
---
+++ Token/migrations/2_upgrade_contract.js
@@ -0,0 +1,15 @@
+// migrations/3_deploy_upgradeable_box.js
+const TransparentUpgradeableProxy = artifacts.require('TransparentUpgradeableProxy');
+const ProxyAdmin = artifacts.require('ProxyAdmin');
+const ExoToken = artifacts.require('ExoToken');
+
+module.exports = async function (deployer) {
+  await deployer.deploy(ExoToken);
+  const exoToken = await ExoToken.deployed();
+  await deployer.deploy(ProxyAdmin);
+  const proxyAdmin = await ProxyAdmin.deployed();
+  await deployer.deploy(TransparentUpgradeableProxy, exoToken.address, proxyAdmin.address, []);
+  const trans = await TransparentUpgradeableProxy.deployed();
+  const proxyExo = await ExoToken.at(trans.address);
+  await proxyExo.initialize();
+};
