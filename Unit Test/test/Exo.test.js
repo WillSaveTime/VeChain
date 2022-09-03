2c9938d2ada527bbe06cc334da01c29be8b9d5d8
cooker0910 <cooker0910@gmail.com>
2022-09-03 10:55:17 -0700

    update

---
+++ Unit Test/test/Exo.test.js
@@ -0,0 +1,21 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { describe, it } = require("node:test");
+
+describe("TokenEth", () => {
+  beforeEach(async() => {
+    const TokenEth = await hre.ethers.getContractFactory("TokenEth");
+    console.log("Deploying TokenEth...")
+    const tokenEth = await upgrades.deployProxy(TokenEth, [])
+  })
+
+  describe("staking", () => {
+    it("should enough EXO token to stake", async() => {
+
+    })
+  })
+})
